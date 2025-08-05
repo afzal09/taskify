0987d98976a15cfcc5a560b1fd16473442330b53
Afzal Momin <afzalmomin2003@gmail.com>
2025-08-06 00:36:18 +0530

    setup task entity and repository

---
+++ lib/features/tasks/domain/repository/task_repository.dart
@@ -0,0 +1,8 @@
+import 'package:whatbytes_assignment/features/tasks/domain/entities/task_entity.dart';
+
+abstract class TaskRepository {
+  Future<void> createTask(TaskEntity task);
+  Future<void> updateTask(TaskEntity task);
+  Future<void> deleteTask(String taskId, String userId);
+  Future<List<TaskEntity>> getUserTasks(String userId);
+}
